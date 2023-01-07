6a7ad1751b392c2e6e63c9deea8550c913f42411
TSuXinH <861411454@qq.com>
2023-01-07 22:56:56 +0800

    new conv

--- net.py
+++ net.py
@@ -37,6 +37,46 @@ class CNN(nn.Module):
         return x
 
 
+class CNN_alter(nn.Module):
+    def __init__(self, k):
+        super().__init__()
+        stride = 1 if k == 500 else 2 if k == 1000 else 3
+        self.conv_block1 = nn.Sequential(
+            nn.Conv1d(4, 64, (1, ), (stride, )),  # shape: [n, 64, 500]
+            nn.BatchNorm1d(64),
+            nn.GELU(),
+            nn.MaxPool1d(2, 2),  # shape: [n, 64, 250]
+        )
+        self.conv_block2 = nn.Sequential(
+            nn.Conv1d(64, 256, (7, ), (4, )),  # shape: [n, 256, 124]
+            nn.BatchNorm1d(256),
+            nn.GELU(),
+            nn.MaxPool1d(2, 2),  # shape: [n, 256, 62]
+        )
+        self.conv_block3 = nn.Sequential(
+            nn.Conv1d(256, 512, (7, ), (4, )),  # shape: [n, 512, 14]
+            nn.BatchNorm1d(512),
+            nn.GELU(),
+            nn.MaxPool1d(2, 2)  # shape: [n, 512, 7]
+        )
+        self.linear_block = nn.Sequential(
+            nn.Linear(512 * 7, 64),
+            nn.Dropout(.2),
+            nn.GELU(),
+            nn.Linear(64, 2000),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        x = x.reshape(-1, 512 * 7)
+        x = self.linear_block(x)
+        return x
+
+
 def train(net, loader, cri, opt, device, max_epoch):
     net.train()
     loss_list = []
